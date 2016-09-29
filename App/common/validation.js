2aa191029522914b314ad443d16c0dc08f9ba39e
cxywind <erickwebsun@gmail.com>
2016-09-29 14:36:14 +1300

    first commit

---
+++ App/common/validation.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    function formValidation() {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+
+                ctrl.$validators.formValidation = function (modelValue, viewValue) {
+                    // the defalut value is form-validation when the validation is not set
+                    if (attrs.formValidation === 'form-validation') {
+                        return true;
+                    }
+                    var valParam = angular.fromJson(attrs.formValidation);
+                    var error = false;
+
+                    if (!error && valParam.required) {
+                        error = modelValue.length > 0 ? false : true;
+                    }
+
+                    if (!error && valParam.minLen) {
+                        error = modelValue.length >= valParam.minLen ? false : true;
+                    }
+
+                    if (!error && valParam.maxLen) {
+                        error= modelValue.length <= valParam.maxLen ? false : true;
+                    }
+
+                    return error ? false: true;
+                };
+            }
+        }
+
+    }
+
+    angular.module('wsCRM').directive("formValidation", formValidation);
+
+})();
---
+++ App/modal/modal-component.js
@@ -0,0 +1,62 @@
+﻿angular.
+  module('crmModal').
+  run(function(editableOptions) {
+      editableOptions.theme = 'bs3'; // xeditable
+  }).
+  component('cModal', {
+      templateUrl: 'App/modal/modal.html',
+      controller: function CModalController($scope, $rootScope,$timeout,$q, utils) {
+          var ws = this;
+          $scope.formShown=false;
+          $scope.modalOption = {};
+          $scope.modalData = [];
+
+          
+          // data is a object array
+          $scope.$on('showModelEvent', function (event, data) {
+              $scope.modalData = data[0];
+              $scope.modalOption = data[1];
+              if ($scope.modalOption.action !== 'index') {
+                  $timeout(function () {
+                      angular.element('#show-form-button').triggerHandler('click');
+                      $scope.formShown = true;
+                  });
+              } else if ($scope.formShown == true) {
+                  $timeout(function () {
+                      angular.element('#cancel-form-button').triggerHandler('click');
+                      $scope.formShown = false;
+                  });
+              }
+          });
+
+          $scope.modalEdit = function () {
+              $scope.formShown = true;
+          };
+
+          // Save modal data to remote database
+          $scope.saveModal = function (data, isvalid) {
+              var d = $q.defer();
+
+              if (isvalid) { 
+                  d.resolve()
+              } else { 
+                  d.resolve('Error')
+                  return d.promise;
+              }
+              
+              if (isvalid) {
+                 
+                  var uri = '/'+$scope.modalOption.controller + '/' + $scope.modalOption.action;
+                  if ($scope.modalOption.idValue) {
+                      uri += '/' + $scope.modalOption.idValue;
+                      data[$scope.modalOption.idVariable] = $scope.modalOption.idValue;
+                  }
+
+                  utils.postApiData(uri, data).then(function (respone) {
+                      $scope.$emit('modelDone', respone); //通知上层component, 已经完成保存，请刷新数据
+                  });
+              }
+          }
+
+      },
+  });
